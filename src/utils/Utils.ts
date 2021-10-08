f7fef90cabfab15764f474c93d3c8e3c42166ee1
PikaYuhno <ahm19409@spengergasse.at>
2021-10-08 17:53:31 +0200

    Cast error to any

--- src/utils/Utils.ts
+++ src/utils/Utils.ts
@@ -18,7 +18,7 @@ export const sendToChannel = async (channel: ThreadChannel | DMChannel, options:
     try {
         return channel.send(options);
     } catch (error) {
-        if (error.code === 500) {
+        if ((error as any).code === 500) {
             if (retries === 5) return;
             Logger.info(`Code 500 error, retrying in 2 seconds the ${retries} time`);
             await new Promise((res, _) => setTimeout(res, 2000));
